034853065da38f4a25d8fda76c4be74688b7da9a
Jeremy Orme <jeremy.orme@gmail.com>
2019-09-02 17:08:40 +0100

    test(classifier): add tests

---
+++ test_classifier.py
@@ -0,0 +1,20 @@
+import pytest
+from pyfathom import *
+
+def test_single_pattern_match():
+	c = classifier('/xyz/ is abc')
+	assert str(c.classify('xyz')) == '<abc>xyz</abc>'
+	
+def test_single_pattern_two_matches():
+	c = classifier('/xyz/ is abc')
+	assert str(c.classify('xyz zyx xyz')) == '<abc>xyz</abc>zyx<abc>xyz</abc>'
+	
+def test_dual_pattern_match():
+	c = classifier('/xyz/,/zyx/ is abc')
+	assert str(c.classify('xyz zyx')) == '<abc>xyz zyx</abc>'
+	
+def test_dual_pattern_no_overlap():
+	c = classifier('/xyz/,/xyz/ is abc')
+	assert str(c.classify('xyz xyz xyz xyz')) == '<abc>xyz xyz</abc><abc>xyz xyz</abc>'
+	
+pytest.main()
