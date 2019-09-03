a4687c01a2be2f984fda78c09ac3157d44e26b18
Jeremy Orme <jeremy.orme@gmail.com>
2019-09-03 06:51:16 +0100

    test(classifier): additional tests

--- test_classifier.py
+++ test_classifier.py
@@ -33,4 +33,30 @@ def test_single_type_match():
 	c = classifier(k)
 	assert str(c.classify('xyz')) == '<abc><def>xyz</def></abc>'
 
+def test_dual_type_match():
+	k = '''
+		/xyz/ is abc
+		/zyx/ is foo
+		abc,foo is def,bar
+	'''
+	c = classifier(k)
+	assert str(c.classify('xyz zyx')) == '<abc><def>xyz</def></abc><foo><bar>zyx</bar></foo>'
+	
+def test_dual_type_match_single_is_type():
+	k = '''
+		/xyz/ is abc
+		/zyx/ is foo
+		abc,foo is ,bar
+	'''
+	c = classifier(k)
+	assert str(c.classify('xyz zyx')) == '<abc>xyz</abc><foo><bar>zyx</bar></foo>'
+	
+def test_type_and_pattern_mix():
+	k = '''
+		/xyz/ is foo
+		/abc/,foo,/cba/ is one,two,three
+	'''
+	c = classifier(k)
+	assert str(c.classify('abc xyz cba')) == '<one>abc</one><foo><two>xyz</two></foo><three>cba</three>'
+	
 pytest.main()
